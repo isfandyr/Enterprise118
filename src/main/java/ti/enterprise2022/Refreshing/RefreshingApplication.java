4bdc4655012dac1f78409d26b0cce186dc4fbd42
Muhammad Adrianto <isfan.diar123@gmail.com>
2022-02-22 14:28:07 +0700

    first commit

---
+++ src/main/java/ti/enterprise2022/Refreshing/RefreshingApplication.java
@@ -0,0 +1,13 @@
+package ti.enterprise2022.Refreshing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefreshingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefreshingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ti/enterprise2022/Refreshing/RefreshingApplicationTests.java
@@ -0,0 +1,13 @@
+package ti.enterprise2022.Refreshing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefreshingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
